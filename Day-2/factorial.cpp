91ffbaa068de31b48f2e5c771a86ec8f799c07e1
aayushbisht <97892535+aayushbisht@users.noreply.github.com>
2023-01-08 14:35:53 +0530

    factorial cpp program added

---
+++ Day-2/factorial.cpp
@@ -0,0 +1,17 @@
+// Program to find factorial of a given number
+#include <iostream>
+using namespace std;
+
+unsigned int factorial(unsigned int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num = 8;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
